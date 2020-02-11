8b4ebf5cacacf741ab968b055c4f5b15bc242567
shiniar <shiniar@163.com>
2020-02-11 10:28:44 +0800

    init repository

---
+++ src/main/java/com/fwj/majiang/community0209/Community0209Application.java
@@ -0,0 +1,13 @@
+package com.fwj.majiang.community0209;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community0209Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Community0209Application.class, args);
+    }
+
+}
